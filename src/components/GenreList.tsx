3d335909e23b39ef6cab24f91b7742956f0a25b5
Adarsh Inaganti <adarshinaganti@Adarsh-M1-Air.local>
2023-05-26 14:09:51 +0530

    implemented genre filtering

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,12 @@
-import {HStack, Image, List, ListItem, Spinner, Text} from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import {Button, HStack, Image, List, ListItem, Spinner} from "@chakra-ui/react";
+import useGenres, {Genre} from "../hooks/useGenres";
 import getCroppedImageURL from "../services/imageURL";
 
-const GenreList = () => {
+interface GenreListProps {
+	onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({onSelectGenre}: GenreListProps) => {
 	const {data, isLoading, error} = useGenres();
 	if (error) return null;
 	if (isLoading) return <Spinner />;
@@ -16,7 +20,13 @@ const GenreList = () => {
 							borderRadius={8}
 							src={getCroppedImageURL(genre.image_background)}
 						/>
-						<Text fontSize="lg">{genre.name}</Text>
+						<Button
+							fontSize="lg"
+							variant="link"
+							onClick={() => onSelectGenre(genre)}
+						>
+							{genre.name}
+						</Button>
 					</HStack>
 				</ListItem>
 			))}
