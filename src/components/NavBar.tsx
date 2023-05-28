cec7b3218e62c022a8ebe37a03fd884c20122230
adarshinaganti <adarshinaganti@Adarsh-M1-Air.local>
2023-05-28 12:32:12 +0530

    implemented searching of games

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,13 @@
 import {HStack, Image} from "@chakra-ui/react";
 import logo from "../assets/Logo/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
-import Search from "./Search";
+import Search, {SearchProps} from "./Search";
 
-const NavBar = () => {
+const NavBar = ({onSearch}: SearchProps) => {
 	return (
 		<HStack padding="10px">
 			<Image src={logo} boxSize="60px" />
-			<Search />
+			<Search onSearch={onSearch} />
 			<ColorModeSwitch />
 		</HStack>
 	);
