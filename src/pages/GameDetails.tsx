c664db442c58191e5f48016037836bf5e28750d0
Adarsh Inaganti <adarshinaganti006@gmail.com>
2023-06-25 10:44:30 +0530

    implemented game attributes

--- src/pages/GameDetails.tsx
+++ src/pages/GameDetails.tsx
@@ -2,6 +2,7 @@ import {Heading, Spinner} from "@chakra-ui/react";
 import {useParams} from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import useFetchGame from "../hooks/useFetchGame";
+import GameAttributes from "../components/GameAttributes";
 
 const GameDetails = () => {
 	const {slug} = useParams();
@@ -14,6 +15,7 @@ const GameDetails = () => {
 		<>
 			<Heading>{game.name}</Heading>
 			<ExpandableText>{game.description_raw}</ExpandableText>
+			<GameAttributes game={game} />
 		</>
 	);
 };
